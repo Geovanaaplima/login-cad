089d46522dca901f8709a836d9568bc89e66c68d
Geovana Aparecida de Lima <lima.geovana@alunoescolassociais.org.br>
2024-07-04 11:06:30 -0300

    javascripst começado

--- controller.js
+++ controller.js
@@ -1,1 +1,12 @@
-  
+//FUNÇÃO PARA VALIDAÇÃO DE ACESSO   
+
+function acessar(){
+    let loginEmail =document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
+
+    if(!loginEmail || !loginSenha){
+        alert("Favor preencher todos os campos");
+    }else{
+        alert("Campos preenchidos com sucesso");
+    }
+}
