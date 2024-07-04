efef216e700fc2d35f1eee804f69cf969d807f56
Geovana Aparecida de Lima <lima.geovana@alunoescolassociais.org.br>
2024-07-04 12:04:21 -0300

    adicionamos no código dadoLista, alert

--- controller.js
+++ controller.js
@@ -7,6 +7,19 @@ function acessar(){
     if(!loginEmail || !loginSenha){
         alert("Favor preencher todos os campos");
     }else{
-        alert("Campos preenchidos com sucesso");
+        //alert("Campos preenchidos com sucesso");
+        window.location.href = 'cadastro.html';
+    }
+}
+
+// FUNÇÃO PARA ARMAZENAMENTO DE NOMES EM ARRAY
+var dadoLista = [];
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+
+    if(nomeUser){
+
+    }else{
+        alert("Favor informe um nome para cadastro");
     }
 }
